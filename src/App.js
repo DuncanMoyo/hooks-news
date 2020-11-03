8b39a9f984ec2408ca5e343ac243af447081f39a
Duncan Moyo <duncanfmoyo@gmail.com>
2020-11-03 16:06:15 +0200

    retching data on component mount : useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+import axios from "axios";
+
+const App = () => {
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://hn.algolia.com/api/v1/search?query=reacthooks")
+      .then((response) => {
+        console.log(response.data);
+        setResults(response.data.hits);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ul>
+        {results.map(({ url, objectID, title }) => (
+          <li key={objectID}>
+            <a href={url}>{title}</a>
+          </li>
+        ))}
+      </ul>
+    </>
   );
-}
+};
 
 export default App;
