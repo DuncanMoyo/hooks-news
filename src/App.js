d3379e14cd01b1b4f1cf2db22d1298784c275070
Duncan Moyo <duncanfmoyo@gmail.com>
2020-11-03 18:11:10 +0200

    styling with tailwind.css

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   const [results, setResults] = useState([]);
   const [query, setQuery] = useState("react hooks");
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null)
+  const [error, setError] = useState(null);
 
   const searchInputRef = useRef();
 
@@ -18,7 +18,7 @@ const App = () => {
       );
       setResults(response.data.hits);
     } catch (error) {
-      setError(error)
+      setError(error);
     }
     setLoading(false);
   };
@@ -38,33 +38,51 @@ const App = () => {
   };
 
   return (
-    <>
-      <form onSubmit={handleSearch}>
+    <div className="container max-w-md mx-auto p-4 m-2 bg-purple-lighest shadow-lg rounded">
+      <img
+        src="https://icon.now.sh/react/c0c"
+        alt="React Logo"
+        className="float-right h-12"
+      />
+      <h1 className="text-grey-darkest font-thin">Hooks News</h1>
+      <form onSubmit={handleSearch} className="mb-2">
         <input
           value={query}
           type="text"
           onChange={(event) => setQuery(event.target.value)}
           ref={searchInputRef}
+          className="border p-1 rounded"
         />
-        <button type="submit">Search</button>
-        <button type="button" onClick={handleClearSearch}>
+        <button className="bg-orange rounded m-1 p-1" type="submit">
+          Search
+        </button>
+        <button
+          className="bg-teal text-white p-1 rounded"
+          type="button"
+          onClick={handleClearSearch}
+        >
           Clear
         </button>
       </form>
 
       {loading ? (
-        <div>Loading results...</div>
+        <div className="font-bold text-orange-dark">Loading results...</div>
       ) : (
-        <ul>
+        <ul className="list-reset leading-normal">
           {results.map(({ url, objectID, title }) => (
             <li key={objectID}>
-              <a href={url}>{title}</a>
+              <a
+                className="text-indigo-dark hover:text-indigo-darkest"
+                href={url}
+              >
+                {title}
+              </a>
             </li>
           ))}
         </ul>
       )}
-      {error && <div>{error.message}</div>}
-    </>
+      {error && <div className="text-red font-bold">{error.message}</div>}
+    </div>
   );
 };
 
